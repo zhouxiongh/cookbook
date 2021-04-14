d16ec164bf42595edce9168ba3f57f27fb082051
hezhoux <hezhoux@qq.com>
2021-04-14 05:20:11 +0000

    计算当月日期范围

---
+++ 3/3.14.py
@@ -0,0 +1,19 @@
+"""
+你的代码需要在当前月份中循环每一天，想找到一个计算这个日期范围的高效方法
+"""
+from datetime import datetime, date, timedelta
+import calendar
+
+def get_month_range(start_date=None):
+    if start_date is None:
+        start_date = date.today().replace(day=1)
+    _, days_in_month = calendar.monthrange(start_date.year, start_date.month)
+    end_date = start_date + timedelta(days=days_in_month)
+    return (start_date, end_date)
+
+if __name__ == "__main__":
+    a_day = timedelta(days=1)
+    first_day, last_day = get_month_range()
+    while first_day < last_day:
+        print(first_day)
+        first_day += a_day
